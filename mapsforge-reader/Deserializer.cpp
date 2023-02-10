931fb5463344a781598013115702aab3b2d4aac6
Linus Schwarz <linus.schwarz@uni-bielefeld.de>
2023-02-10 14:25:28 +0100

    Code formatting

--- mapsforge-reader/Deserializer.cpp
+++ mapsforge-reader/Deserializer.cpp
@@ -18,28 +18,28 @@
 namespace carto {
     int32_t Deserializer::getInt(const std::vector<char> &data, int offset) {
         return (int32_t)(data[offset] << 24
-                         | (data[offset + 1] & 0xff) << 16
-                         | (data[offset + 2] & 0xff) << 8
-                         | (data[offset + 3] & 0xff));
+            | (data[offset + 1] & 0xff) << 16
+            | (data[offset + 2] & 0xff) << 8
+            | (data[offset + 3] & 0xff));
     }
 
     uint64_t Deserializer::getFiveBytesLong(const std::vector<char> &data, int offset) {
         return (data[offset] & 0xffL) << 32
-               | (data[offset + 1] & 0xffL) << 24
-               | (data[offset + 2] & 0xffL) << 16
-               | (data[offset + 3] & 0xffL) << 8
-               | (data[offset + 4] & 0xffL);
+            | (data[offset + 1] & 0xffL) << 24
+            | (data[offset + 2] & 0xffL) << 16
+            | (data[offset + 3] & 0xffL) << 8
+            | (data[offset + 4] & 0xffL);
     }
 
     int64_t Deserializer::getLong(const std::vector<char> &data, int offset) {
         return (int64_t)(data[offset] & 0xffL) << 56
-                        | (data[offset + 1] & 0xffL) << 48
-                        | (data[offset + 2] & 0xffL) << 40
-                        | (data[offset + 3] & 0xffL) << 32
-                        | (data[offset + 4] & 0xffL) << 24
-                        | (data[offset + 5] & 0xffL) << 16
-                        | (data[offset + 6] & 0xffL) << 8
-                        | (data[offset + 7] & 0xffL);
+            | (data[offset + 1] & 0xffL) << 48
+            | (data[offset + 2] & 0xffL) << 40
+            | (data[offset + 3] & 0xffL) << 32
+            | (data[offset + 4] & 0xffL) << 24
+            | (data[offset + 5] & 0xffL) << 16
+            | (data[offset + 6] & 0xffL) << 8
+            | (data[offset + 7] & 0xffL);
     }
 
     int16_t Deserializer::getShort(const std::vector<char> &data, int offset) {
